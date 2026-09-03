246a1d1364cac5e06f9f57deb3671ac547b59218
johndelker <jdelker@msu.edu>
2026-09-03 13:43:59 -0400

    Fix issue with web app

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 // Keep this as the single source of truth for your GAS Web App endpoint.
-window.WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbz2Bm7Ivi5bDNONfzCiiRyh1y2Wj4CHYdkmU5zhkr7rNmk06nfx_UJAKXamUeEkjVaTDQ/exec';
+window.WEB_APP_URL = 'https://script.google.com/macros/s/AKfycby4uKWBp4cpVJhd87jlWNMYTx6YOyCNpnD9D-0lyf1Oavxxc0wxPRkQTvLTArFBnIdF4Q/exec';
